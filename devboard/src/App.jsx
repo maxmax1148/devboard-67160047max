072d0a18e06d232fd73eb641dec586985869f0d4
67160047 <67160047@go.buu.ac.th>
2026-03-31 01:10:22 +0700

    fix: configure Vercel to use Vite build instead of VitePress

--- devboard/src/App.jsx
+++ devboard/src/App.jsx
@@ -1,12 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FavoritesProvider } from "./context/FavoritesContext";
 import Navbar from "./components/Navbar";
-import PostCard from "./components/PostCard";
+import HomePage from "./pages/HomePage";
+import PostDetailPage from "./pages/PostDetailPage";
+import ProfilePage from "./pages/ProfilePage";
+import FavoritesPage from "./pages/FavoritesPage";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <PostCard title="ทดสอบ" body="นี่คือเนื้อหา" />
-    </div>
+    <FavoritesProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/posts/:id" element={<PostDetailPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </BrowserRouter>
+    </FavoritesProvider>
   );
 }
 
